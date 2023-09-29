48583ee0efcf5b6637d5e023e4eb88e871e6040b
v.kruglikov <vladkryglikov@mail.ru>
2023-09-30 02:50:35 +0300

    edit output

--- ChatApp/ChatApp/Hubs/ChatHub.cs
+++ ChatApp/ChatApp/Hubs/ChatHub.cs
@@ -4,15 +4,15 @@ namespace ChatApp.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task JoinRoom(string roomName)
+        public async Task JoinRoom(string roomName, string name)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
-            await Clients.Group(roomName).SendAsync("Notify", $"has joined the group {roomName}");
+            await Clients.Group(roomName).SendAsync("Notify", $"{name} has joined the group {roomName}");
         }
 
-        public async Task LeaveRoom(string roomName) 
+        public async Task LeaveRoom(string roomName, string name) 
         {
-            await Clients.Group(roomName).SendAsync("Notify", $"has left the group {roomName}");
+            await Clients.Group(roomName).SendAsync("Notify", $"{name} has left the group {roomName}");
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
         }
 
